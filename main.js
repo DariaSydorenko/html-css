8b1dabae985ed71c5fd3e8ef764ce96bdb0f67eb
DariaSydorenko <104034908+DariaSydorenko@users.noreply.github.com>
2023-06-02 23:37:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+/*const burgerIcon = document.querySelector('.burger');
+const navMenu = document.querySelector('nav');
+
+burgerIcon.addEventListener('click', function() {
+  navMenu.classList.toggle('show');
+});
+
+nav.addEventListener('click', function() {
+    nav.classList.remove('show');
+});*/
+/*$(document).ready(function() {
+  $('.burger').clip(function(event) {
+    $('.burger, .nav').toggleClass('active');
+  });
+});*/
+
+/*$(document).ready(function() {
+  $('.burger').click(function(event) {
+    $('.burger, .burger-nav').toggleClass('active');
+  });
+
+  $('burger-nav').click(function() {
+    $('.burger-nav').removeClass('active');
+  });
+});*/
